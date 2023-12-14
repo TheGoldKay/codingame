d73f4b1d7b2847caeee5dea11349e5938afc330f
TheGoldKay <jhonnatanwfc@live.com>
2023-12-14 14:28:15 -0300

    binary search

--- medium/bat_bomb.py
+++ medium/bat_bomb.py
@@ -12,31 +12,65 @@ n = int(input())  # maximum number of turns before game over.
 x, y = [int(i) for i in input().split()]
 
 mov = {"U": (0, -1), "UR": (1, -1), "R": (1, 0), "DR": (1, 1), "D": (0, 1), "DL": (-1, 1), "L": (-1, 0), "UL": (-1, -1)}
-pos = []
-def countDir(lst):
-    c = 1
-    ln = len(lst)
-    if(ln >= 2):
-        i = ln - 2
 
-        while(lst[ln - 1] == lst[i] and i > 0):
-            c += 1
-            i -= 1
-    return c
+# Binary search algorithm to find the position of Batman
+# This function assumes that 'x', 'y', 'w', 'h', and 'bomb_dir' are available in the scope
+# where this function is called.
+
+def binary_search(x, y, w, h, bomb_dir):
+    # Define the search space
+    left, right = 0, w - 1
+    top, bottom = 0, h - 1
+    
+    # Continue until the search space is valid
+    while left <= right and top <= bottom:
+        # Calculate the middle of the current search space
+        mid_x = (left + right) // 2
+        mid_y = (top + bottom) // 2
+        
+        # Narrow down the search space based on the bomb's direction
+        if 'L' in bomb_dir:
+            right = mid_x - 1
+        if 'R' in bomb_dir:
+            left = mid_x + 1
+        if 'U' in bomb_dir:
+            bottom = mid_y - 1
+        if 'D' in bomb_dir:
+            top = mid_y + 1
+        
+        # Update Batman's position to the middle of the new search space
+        x = (left + right) // 2
+        y = (top + bottom) // 2
+    
+    # Return the updated position
+    return x, y
+
+def search(bomb_dir):
+    global left, right, up, down
+    mid_x, mid_y = x, y
+    if('L' in bomb_dir):
+        mid_x = (x - left) // 2
+        left = mid_x
+        print(f"LEFT {mid_x}", file=sys.stderr, flush=True)
+    elif('R' in bomb_dir):
+        mid_x = x + (right - x) // 2
+        right = mid_x
+        print(f"RIGHT {mid_x}", file=sys.stderr, flush=True)
+    if('U' in bomb_dir):
+        mid_y = (y - up) // 2
+        up = mid_y
+        print(f"UP {mid_y}", file=sys.stderr, flush=True)
+    elif('D' in bomb_dir):
+        mid_y = y + (down - y) // 2
+        down = mid_y
+        print(f"DOWN {mid_y}", file=sys.stderr, flush=True)
+    return mid_x, mid_y
+
+    
 while True:
     bomb_dir = input()  # the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
-    pos.append(bomb_dir)
-    # Write an action using print
-    print(f"{bomb_dir} - {mov[bomb_dir]}", file=sys.stderr, flush=True)
 
-
-    # the location of the next window Batman should jump to.
-    dx, dy = mov[bomb_dir]
-    x += dx * countDir(pos)
-    y += dy * countDir(pos)
-    if(x >= w):
-        x = w - 1
-    if(y >= h):
-        y = h - 1
+    # Update Batman's position using binary search
+    x, y = search(x, y, w, h, bomb_dir)
     
     print(f"{x} {y}")
